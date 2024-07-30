9671e089497b749665442a8c1b3c68c5ffbd52bd
ibrahimFQT <162083376+ibrahimFQT@users.noreply.github.com>
2024-07-30 11:44:41 +0700

    meet 2

---
+++ meeting2/basicjs.js
@@ -0,0 +1,24 @@
+//soal number 1
+let hapes = "turky bau ketek"
+console.log(hapes);
+
+//soal number 2
+let nama = "ibrahim";
+let umur = "16";
+let tempattinggal = "bintaro,tangsel";
+
+console.log(nama);
+console.log(umur);
+console.log(tempattinggal);
+
+//soal number 3
+var a = "null";
+console.log(a);
+
+//soal number 4
+let alas = 5;
+let tinggi = 7;
+let rumus = 1/2;
+let hasil = alas * tinggi * rumus;
+
+hasil
